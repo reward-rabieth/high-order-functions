a5086d3549c6b12e6fa8803d965a0fd5f0301f03
reward-rabieth <rewardrabieth06@gmail.com>
2022-07-02 16:33:29 +0300

    first commit

---
+++ reduce.js
@@ -0,0 +1,62 @@
+/*
+ * Reduce is the trickiest of the methods and the most controversial.
+ * Reduce will return a new value and should be used to reduce an array
+ * of data down to a single value
+ */
+/*
+ * Find the sum of all user's age
+ */
+
+
+import { faker } from '@faker-js/faker';
+
+const users=Array(10).fill(undefined).map(()=>{
+    return{
+        id:faker.random.numeric(),
+firstName:faker.name.findName(),
+secondName:faker.name.lastName(),
+age:faker.datatype.number(100),
+
+
+    }
+})
+
+console.log(users)
+
+const sum=users.reduce((acc,curr)=>{
+
+
+acc= acc+ curr.age
+return acc
+},0)
+
+console.log(sum);
+
+/*
+ * Get the average age of all users
+ */
+
+
+const average=users.reduce((acc,curr,index)=>{
+    acc+=curr.age;
+    if(index===users.length-1){
+return acc/users.length
+    }else{
+   return acc;
+    }
+
+
+
+},0);
+
+console.log({average},sum/users.length)
+
+
+/*
+ * Transform the array of users into an object with the user's id as the key
+ */
+const userObject=users.reduce((acc,curr)=>{
+acc[curr.id]=curr
+    return acc
+},{})
+console.log(userObject)
